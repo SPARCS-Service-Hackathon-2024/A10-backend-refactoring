52ff0ee9927223198797729d312b5ce62fd3744d
suri-pu-bi <ymg12347@gmail.com>
2024-02-22 15:45:18 +0900

    init: project initial

---
+++ src/main/java/com/hackathon/app/A10backend/A10BackendApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.app.A10backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A10BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A10BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/app/A10backend/A10BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.app.A10backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A10BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
